d4516ff07183714e36006e75f056ab0451e2c471
TejaswiniVechalapu <tvechalapu@linkfields.com>
2026-05-26 11:27:17 +0530

    chang ethe dahboard api name

--- Broker-Portal-Frontend/lib/api/dashboard.ts
+++ Broker-Portal-Frontend/lib/api/dashboard.ts
@@ -1,7 +1,7 @@
 import { apiClient } from "./apiClient";
 export async function getDashboardMetrics(): Promise<any> {
   const json = await apiClient<{ success: boolean; data: any }>(
-    `/broker/dashboard/status`,
+    `/broker/dashboard/metrics`,
     { cache: "no-store" }
   );
   return json.data;
